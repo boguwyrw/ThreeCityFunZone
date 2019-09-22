c38fbd6f7c1e29167b2a4091ca40092be00faa46
boguwyrw <b.wyrwal@wp.pl>
2019-09-22 13:50:07 +0200

    Users registration - in progress Styles on page Login and page Register in progress

--- src/main/java/pl/threecityfunzone/controller/FunZoneUserController.java
+++ src/main/java/pl/threecityfunzone/controller/FunZoneUserController.java
@@ -10,7 +10,7 @@ import pl.threecityfunzone.model.FunZoneUser;
 import pl.threecityfunzone.service.FunZoneUserService;
 
 @Controller
-@RequestMapping(path = "/UsersRegisterLoginPage")
+//@RequestMapping(path = "/UsersRegisterLoginPage")
 public class FunZoneUserController {
 
     private String pageTitle = "";
@@ -18,20 +18,20 @@ public class FunZoneUserController {
     @Autowired
     private FunZoneUserService funZoneUserService;
 
-    @GetMapping(path = "/UsersRegisterPage")
+    @GetMapping(path = "/UsersRegisterLoginPage/UsersRegisterPage")
     public String usersRegisterPage (Model model) {
         pageTitle = "Users Register Page";
         model.addAttribute("pageTitle", pageTitle);
         return "UsersRegisterPage";
     }
 
-    @PostMapping(path = "/UsersRegisterPage")
+    @PostMapping(path = "/UsersRegisterLoginPage/UsersRegisterPage")
     public String usersRegisterPage(Model model, FunZoneUser funZoneUser){
         funZoneUserService.saveFunZoneUser(funZoneUser);
         return "redirect:/UsersRegisterLoginPage";
     }
 
-    @GetMapping(path = "/UsersLoginPage")
+    @GetMapping(path = "/UsersRegisterLoginPage/UsersLoginPage")
     public String usersLoginPage (Model model) {
         pageTitle = "Users Login Page";
         model.addAttribute("pageTitle", pageTitle);
--- src/main/java/pl/threecityfunzone/model/FunZoneUser.java
+++ src/main/java/pl/threecityfunzone/model/FunZoneUser.java
@@ -1,10 +1,19 @@
 package pl.threecityfunzone.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class FunZoneUser extends Person {
 
     @Id
